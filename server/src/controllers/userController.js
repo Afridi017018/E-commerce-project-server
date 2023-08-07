463706c8da533de1e145299cc868cf207fd21c9c
Afridi017018 <93644242+Afridi017018@users.noreply.github.com>
2023-08-07 21:57:33 +0600

    implemented search , pagination, get single user api etc

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,16 +1,90 @@
 const createError = require('http-errors');
-const { users } = require('../models/userModel');
+const mongoose = require('mongoose');
+const User = require('../models/userModel');
+const { successResponse } = require('./responseController');
 
 
-const getUsers = (req,res)=>{
+
+const getUsers = async (req,res,next)=>{
     try {
-        res.status(200).json({
-            message: "user profile is returned",
-            users: users,
-        });
+
+        const search = req.query.search || '';
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 5;
+        
+        const searchRegExp = new RegExp('.*'+search+'.*', 'i');
+
+        const filter = {
+            isAdmin: {$ne : true},
+
+            $or:[
+                {name: {$regex: searchRegExp} },
+                {email: {$regex: searchRegExp} },
+                {phone: {$regex: searchRegExp} }
+            ]
+        };
+
+        const options = {password: 0};
+
+        const users = await User.find(filter,options)
+        .limit(limit)
+        .skip( (page-1)*limit )
+
+        const count = await User.find(filter).countDocuments();
+
+        if(!users) throw createError(404, 'no user found');
+
+
+
+        return successResponse(res,{
+            statusCode: 200,
+            message: 'user profile is returned',
+            payload:{
+                users: users,
+                pagination:{
+                totalPages: Math.ceil(count / limit),
+                currentPage: page,
+                previousPage: page-1 > 0 ? page-1 : null,
+                nextPage: page+1 <= Math.ceil(count / limit) ? page+1 : null,
+
+            }
+            }
+        })
+
     } catch (error) {
         next(error);
     };
 };
 
-module.exports = {getUsers};
+
+const getUser = async (req,res,next)=>{
+    try {
+
+        const id = req.params.id;
+        const options = {password: 0};
+
+        const user = await User.findById(id, options)
+
+        if(!user)
+        {
+            throw createError(404, 'User does not exist with this id');
+        }
+
+        return successResponse(res,{
+            statusCode: 200,
+            message: 'user profile is returned',
+            payload:{
+                user,
+            }
+        })
+
+    } catch (error) {
+        if(error instanceof mongoose.Error)
+        {
+            next(createError(400, 'Invalid user id'))
+        }
+        next(error);
+    };
+};
+
+module.exports = {getUsers, getUser};
--- server/src/routers/userRouter.js
+++ server/src/routers/userRouter.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userRouter = express.Router();
 
-const {getUsers} = require('../controllers/userController')
+const {getUsers, getUser} = require('../controllers/userController')
 
 
 userRouter.get('/', getUsers)
+userRouter.get('/:id', getUser)
+
 
 module.exports = {userRouter}
